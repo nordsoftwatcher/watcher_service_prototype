4e75b496f1b849cbca497780af720b913a55745d
Dmitriy Konoplev <konoplev@yarnord.ru>
2018-11-19 14:59:50 +0300

    Added blank route service

---
+++ backend/services/route-service/src/main/java/ru/nord/siwatch/backend/services/route/Application.java
@@ -0,0 +1,23 @@
+package ru.nord.siwatch.backend.services.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "ru.nord.siwatch.backend.services")
+@EntityScan(basePackages = "ru.nord.siwatch.backend.services.route.entities")
+@EnableJpaRepositories(basePackages = "ru.nord.siwatch.backend.services.route.repositories")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
