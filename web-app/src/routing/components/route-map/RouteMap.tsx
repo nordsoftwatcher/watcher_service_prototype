051eae33cc877df88068f6c70587901ba1e56ba2
Viktor Smirnov <smirnov@yarnord.ru>
2018-11-22 19:04:14 +0300

    ui

--- web-app/src/routing/components/route-map/RouteMap.tsx
+++ web-app/src/routing/components/route-map/RouteMap.tsx
@@ -8,13 +8,30 @@ import { IRouteInstance } from '../../models/route-instance';
 import { routePointIcon, passedPointIcon, currentPosIcon } from './icons';
 import { UUID } from '../../models/uuid';
 import { getDeviations } from '../../utils/deviations';
+import { Coordinates } from '../../models/coordinates';
 
 interface RouteMapProps {
   route: IRoute;
   routeInstance?: IRouteInstance;
 }
 
-export class RouteMap extends React.Component<RouteMapProps> {
+interface RouteMapState {
+  zoom: number;
+  center: Coordinates;
+}
+
+export class RouteMap extends React.Component<RouteMapProps, RouteMapState> {
+
+  constructor(props: RouteMapProps) {
+    super(props);
+
+    const { route, routeInstance } = this.props;
+
+    this.state = {
+      center: routeInstance && routeInstance.currentPos || route.checkpoints[0].coords,
+      zoom: 15,
+    };
+  }
 
   getPointById(pointId: UUID) {
     return this.props.route.checkpoints.find(x => x.id === pointId);
@@ -22,13 +39,12 @@ export class RouteMap extends React.Component<RouteMapProps> {
 
   render() {
     const { route, routeInstance } = this.props;
-    const centerMap = routeInstance && routeInstance.currentPos || route.checkpoints[0].coords;
 
     const deviations = getDeviations(routeInstance);
 
     return (
       <div className={styles.root}>
-        <Map center={centerMap} zoom={15}>
+        <Map center={this.state.center} zoom={this.state.zoom}>
           <TileLayer
             attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -65,7 +81,7 @@ export class RouteMap extends React.Component<RouteMapProps> {
           )}
 
           {routeInstance && routeInstance.currentPos &&
-            <Marker position={routeInstance.currentPos} icon={currentPosIcon} />
+            <Marker position={routeInstance.currentPos} icon={currentPosIcon} zIndexOffset={1000} />
           }
         </Map>
       </div>
